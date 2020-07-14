7cf53707b803bb4504314250ed7bdab8f2d3dca0
zekroTJA <riho@live.de>
2020-07-14 12:48:26 +0200

    update version

--- src/main/java/de/zekro/fdrpc/ForgeDiscordRPC.java
+++ src/main/java/de/zekro/fdrpc/ForgeDiscordRPC.java
@@ -15,7 +15,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class ForgeDiscordRPC {
     public static final String MOD_ID = "fdrpc";
     public static final String NAME = "ForgeDiscordRPC";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.0";
 
     private static Config config;
 
