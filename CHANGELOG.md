dd674aa319a10b6a4a40b87e99dd6f0f6911ca6a
Ringo Hoffmann <riho@live.de>
2021-02-18 12:46:17 +0100

    update release cd and add changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+[version will be automatically inserted here by CD]
+
+- Dimension names are now completely overridable in the 
+  config by prefixing the value with `!`. This way older
+  configs are still compatible.
+  
+- A key bind was added which can be utilized to enable or
+  disable Rich Presence.  
+  ![](https://i.imgur.com/5r2axa6.png)
+
+- Update to Forge 14.23.5.2854
--- src/main/java/de/zekro/fdrpc/ForgeDiscordRPC.java
+++ src/main/java/de/zekro/fdrpc/ForgeDiscordRPC.java
@@ -20,7 +20,7 @@ import org.lwjgl.input.Keyboard;
 public class ForgeDiscordRPC {
     public static final String MOD_ID = "fdrpc";
     public static final String NAME = "ForgeDiscordRPC";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.2.0";
 
     public static KeyBinding toggleKeyBinding = new KeyBinding("key.toggleactive", Keyboard.KEY_NONE, "key.category.main");
 
